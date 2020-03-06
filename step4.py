a80503e7c0062a413401e8bbbeda351931724b54
Jean-Sebastien BEVILACQUA <realitix@gmail.com>
2020-03-06 17:49:31 +0100

    Update

--- step4.py
+++ step4.py
@@ -8,8 +8,6 @@ def run():
     aliment_files = set(listdir(path.join(HERE, 'cache', 'step3')))
     aliment_files = aliment_files - set(['traductions'])
     result = {
-        'map': {},
-        'categories_fr': {},
         'aliments': [],
         'timestamp': get_timestamp()
     }
@@ -19,40 +17,6 @@ def run():
     for a in aliment_files:
         with open(path.join(HERE, 'cache', 'step3', a), 'r') as f:
             aliments.append(json.load(f))
-
-    # Retrieve categories and nutrients to create id
-    def add_in_map(map, elem):
-        if elem not in map:
-            map[elem] = len(map) + 1
-
-    data_map = {}
-    for a in aliments:
-        add_in_map(data_map, a['category']) # category is in english
-        for k, v in a['nutrition'].items():
-            for e in v:
-                nutrion_name = e['name']
-                add_in_map(data_map, nutrion_name)
-    result['map'] = data_map
-
-    # Traduce categories in French
-    try:
-        with open(path.join(HERE, 'cache', 'step4', 'categories_traduction'), 'r') as f:
-            cache_traduction = json.load(f)
-    except:
-        cache_traduction = {'': ''}
-
-    translator = Translator()
-    for k, v in result['map'].items():
-        if k not in cache_traduction:
-            trad = translator.translate(k, src='en', dest='fr')
-            fr_val = trad.text
-            cache_traduction[k] = fr_val
-        else:
-            fr_val = cache_traduction[k]
-        result['categories_fr'][k] = fr_val
-
-    with open(path.join(HERE, 'cache', 'step4', 'categories_traduction'), 'w') as f:
-        json.dump(cache_traduction, f)
 
     # Add aliments to result
     for a in aliments:
@@ -71,18 +35,18 @@ def run():
                 r['names_fr'].append((n['val'], n['rank']))
 
         # category
-        r['category_id'] = result['map'][a['category']]
+        r['category'] = a['category']
 
         # measures
-        r['measures'] = {}
+        r['measures'] = []
         for m in a['measures']:
-            r['measures'][m['name']] = m['value']
+            r['measures'].append((m['name'], m['value']))
 
         # nutritions
-        r['nutritions'] = {}
+        r['nutritions'] = []
         for k, v in a['nutrition'].items():
-            for e in v:
-                r['nutritions'][result['map'][e['name']]] = {'val': e['value'], 'unit': e['unit']}
+            for n in v:
+                r['nutritions'].append(n)
         
         # add to result
         result['aliments'].append(r)
